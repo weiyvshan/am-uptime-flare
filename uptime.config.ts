4dba430f0e2598c3d733ccc2f8a96108465cd527
weiyvshan <143877240+weiyvshan@users.noreply.github.com>
2024-10-23 11:06:15 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,12 +1,12 @@
 const pageConfig = {
   // Title for your status page
-  title: "AM科技's Status Page",
+  标题: "AM科技's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://am.809098.xyz', label: '个人博客', highlight: true },
-    { link: 'https://youtube.com/@AM_CLUB', label: 'AM科技' },
-    { link: 'https://github.com/amclubs', label: 'GitHub' },
-    { link: 'https://809098.xyz', label: 'Blog'},
+    // { link: 'https://am.809098.xyz', 标签: '个人博客', highlight: true },
+    // { link: 'https://youtube.com/@AM_CLUB', 标签: 'AM科技' },
+    // { link: 'https://github.com/amclubs', 标签: 'GitHub' },
+    // { link: 'https://809098.xyz', 标签: 'Blog'},
   ],
 }
 
@@ -17,47 +17,54 @@ const workerConfig = {
   // passwordProtection: 'username:password',
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
+    // // Example HTTP Monitor
+    // {
+    //   // `id` should be unique, history will be kept if the `id` remains constant
+    //   id: 'am.809098.xyz',
+    //   // `name` is used at status page and callback message
+    //   名字: '个人博客',
+    //   // `method` should be a valid HTTP Method
+    //   method: 'GET',
+    //   // `target` is a valid URL
+    //   target: 'https://am.809098.xyz',
+    //   // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+    //   tooltip: 'This is a tooltip for this monitor',
+    //   // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+    //   statusPageLink: 'https://am.809098.xyz',
+    //   // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+    //   // expectedCodes: [200],
+    //   // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+    //   timeout: 10000,
+    //   // [OPTIONAL] headers to be sent
+    //   // headers: {
+    //   //   'User-Agent': 'Uptimeflare',
+    //   //   Authorization: 'Bearer YOUR_TOKEN_HERE',
+    //   // },
+    //   // [OPTIONAL] body to be sent
+    //   // body: 'Hello, world!',
+    //   // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+    //   // responseKeyword: 'success',
+    //   // [OPTIONAL] if specified, the check will run in your specified region,
+    //   // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+    //   // checkLocationWorkerRoute: 'https://am.809098.xyz',
+    // },
+    // // Example TCP Monitor
+    // {
+    //   id: '809098.xyz',
+    //   名字: 'Blog',
+    //   // `method` should be `TCP_PING` for tcp monitors
+    //   method: 'GET',
+    //   // `target` should be `host:port` for tcp monitors
+    //   target: 'https://809098.xyz',
+    //   tooltip: 'My production server monitor',
+    //   statusPageLink: 'https://809098.xyz',
+    //   timeout: 10000,
+    // },
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'am.809098.xyz',
-      // `name` is used at status page and callback message
-      name: '个人博客',
-      // `method` should be a valid HTTP Method
+      id: 'qinglong',
+      name: '青龙',
       method: 'GET',
-      // `target` is a valid URL
-      target: 'https://am.809098.xyz',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://am.809098.xyz',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      // expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      // headers: {
-      //   'User-Agent': 'Uptimeflare',
-      //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-      // },
-      // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://am.809098.xyz',
-    },
-    // Example TCP Monitor
-    {
-      id: '809098.xyz',
-      name: 'Blog',
-      // `method` should be `TCP_PING` for tcp monitors
-      method: 'GET',
-      // `target` should be `host:port` for tcp monitors
-      target: 'https://809098.xyz',
-      tooltip: 'My production server monitor',
-      statusPageLink: 'https://809098.xyz',
+      target: 'https://weiyv-qinglong.hf.space',
       timeout: 10000,
     },
   ],
@@ -104,4 +111,4 @@ const workerConfig = {
 }
 
 // Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }
+输出 { pageConfig, workerConfig }
